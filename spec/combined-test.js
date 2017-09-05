1e31bf4f37ea6700007884ca835a81955921e553
Joortiz <joortiz@adobe.com>
2017-09-05 08:54:18 -0700

    init commit

---
+++ spec/combined-test.js
@@ -0,0 +1,22 @@
+var api = require('../main'),
+  testData = require('../test-data');
+
+describe('The numberOfChars function ', function () {
+  describe('counts the chars ', function () {
+    it('in poem', function () {
+      var result = api.numberOfChars(testData.poem);
+
+      result.should.eql(20);
+    });
+  });
+});
+
+describe('The numberOfCertainChars function ', function () {
+  describe('counts the number of As ', function () {
+    it('in poem', function () {
+      var result = api.numberOfCertainChars(testData.poem, 'a');
+
+      result.should.eql(6);
+    });
+  });
+});
